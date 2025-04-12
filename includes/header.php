2aae674149dcd7de51418aa16bd9eb0434bd7abb
raedtareq1 <h.t.y.a.2001@gmail.com>
2025-04-12 11:54:48 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./layout/css/css_select2.min.css">
-    <link rel="stylesheet" href="./layout/css/font-awesome.css">
-    <link rel="stylesheet" href="./layout/css/fontawesome.min.css">
-    <link rel="stylesheet" href="./layout/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./layout/css/style.css">
+    <link rel="stylesheet" href="layout/css/css_select2.min.css">
+    <link rel="stylesheet" href="layout/css/font-awesome.css">
+    <link rel="stylesheet" href="layout/css/fontawesome.min.css">
+    <link rel="stylesheet" href="layout/css/bootstrap.min.css">
+    <link rel="stylesheet" href="layout/css/style.css">
     <title><?php git_title($pageTitle);?></title>
 </head>
 <body>
